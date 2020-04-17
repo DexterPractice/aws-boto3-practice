6e90ce1a674b9a5bb70972a7f5426114d7949fc6
DexterCandelaria <candelariadexter@yahoo.com>
2020-04-17 20:33:04 +0800

    Added Volume Clean-up

--- testrun.py
+++ testrun.py
@@ -2,7 +2,6 @@ import boto3
 
 session = boto3.Session(profile_name='default')
 
-account_id = boto3.client('sts').get_caller_identity().get('Account')
 ec2 = boto3.client('ec2')
 
 regions = [region['RegionName']
@@ -10,17 +9,11 @@ regions = [region['RegionName']
 
 for region in regions:
     print("Region:", region)
-    ec2 = boto3.client('ec2', region_name=region)
-    response = ec2.describe_snapshots(OwnerIds=[account_id])
-    snapshots = response["Snapshots"]
-    snapshots.sort(key=lambda x: x["StartTime"])
-    snapshots = snapshots[:-1]
+    ec2 = boto3.resource('ec2', region_name=region)
 
-    for snapshot in snapshots:
-            id = snapshot['SnapshotId']
-            try:
-                print("Deleting snapshot:", id)
-                ec2.delete_snapshot(SnapshotId=id)
-            except Exception as e:
-                print("Snapshot {} in use, skipping.".format(id))
-                continue
+    volumes = ec2.volumes.filter(
+        Filters=[{'Name': 'status', 'Values': ['available']}])
+    for volume in volumes:
+        v = ec2.Volume(volume.id)
+        print("Deleting EBS volume: {}, Size: {} GiB".format(v.id, v.size))
+        v.delete()
